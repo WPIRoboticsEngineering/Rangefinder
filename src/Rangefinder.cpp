0b724194defcb6ad7ef3ce7dca7eb82e7ee36f98
Greg <lewin.gc@gmail.com>
2022-10-01 15:21:47 -0400

    broke into base/derived classes...not tested!

--- src/Rangefinder.cpp
+++ src/Rangefinder.cpp
@@ -1,44 +1,4 @@
 #include <Rangefinder.h>
-#include <PCint.h>
-
-#define ECHO_RECD   0x02
-
-/** \brief Constructor.
- * 
- * @param echo The echo pin. Must be interrupt enabled. PCInts OK.
- * @param trig The trigger pin.
- * */
-Rangefinder::Rangefinder(uint8_t echo, uint8_t trig) 
-{
-    echoPin = echo;
-    trigPin = trig;
-}
-
-// sets up the interface
-void Rangefinder::init(void)
-{
-  // ensure ECHO pin is an input
-  pinMode(echoPin, INPUT);
-
-  // register the interrupt for the echo
-  if(digitalPinToInterrupt(echoPin) != NOT_AN_INTERRUPT)
-  {
-    Serial.println("Attaching rangefinder ISR");
-    attachInterrupt(digitalPinToInterrupt(echoPin), ::ISR_Rangefinder, CHANGE);
-  }
-  else if(digitalPinToPCInterrupt(echoPin) != NOT_AN_INTERRUPT)
-  {
-    Serial.println("Attaching rangefinder PC_ISR");
-    attachPCInt(digitalPinToPCInterrupt(echoPin), ::ISR_Rangefinder);
-  }
-  else
-  {
-    Serial.println("Not a rangefinder interrupt pin!");
-  }
-
-  //control pin for commanding pings must be an output
-  pinMode(trigPin, OUTPUT);
-}
 
 /**
  * \brief checkPingTimer() checks to see if it's time to send a new ping.
@@ -48,82 +8,15 @@ void Rangefinder::init(void)
  * 
  * getDistance() calls this function, so you don't need to call this function manually.
  */
-uint8_t Rangefinder::checkPingTimer(void)
-{
-    // if the echo pin is still high, just update the last ping time
-    if(digitalRead(echoPin)) lastPing = millis();
-
-    // check if we're ready to ping
-    if(millis() - lastPing >= pingInterval)
-    {
-        //disable interrupts while we adjust the ISR variables
-        //would be better to just disable this one interrupt, but it would be messy
-        cli();
-        pulseEnd = pulseStart = 0;
-
-        //clear out any leftover states
-        state = 0;
-        sei();
-
-        // keep track of when we sent the last ping
-        lastPing = millis();  
-
-        // toggle the trigger pin to send a chirp
-        digitalWrite(trigPin, HIGH); //commands a ping; leave high for the duration
-        delayMicroseconds(30); //datasheet says hold HIGH for >20us; we'll use 30 to be 'safe'
-        digitalWrite(trigPin, LOW); //unclear if pin has to stay HIGH
-    }
-
-    return state;
-}
-
-uint16_t Rangefinder::checkEcho(void)
-{
-    uint16_t echoLength = 0;
-    cli();
-    if(state & ECHO_RECD)
-    {
-        echoLength = pulseEnd - pulseStart;
-        state &= ~ECHO_RECD;
-    }
-    sei();
-
-    return echoLength;
-}
-
-bool Rangefinder::getDistance(float& distance)
+bool Rangefinder::checkReadingTimer(void)
 {
     bool retVal = false;
-
-    uint16_t pulseDur = checkEcho();
-    if(pulseDur) retVal = true;
-    
-    // nominal translation as given by the datasheet; adjust as needed
-    distance = pulseDur / 58.0;
-
-    // After we've checked for an echo, check to send the next ping
-    checkPingTimer();
+    // check if we're ready to ping
+    if(millis() - lastReading >= readingInterval)
+    {
+        lastReading = millis();  
+        retVal = true;
+    }
 
     return retVal;
 }
-
-/** \brief ISR for the echo pin
- * 
- * Records both the start and stop (rise and fall) of the echo pin.
- * When the pin goes low, it sets a flag.
- * */
-void Rangefinder::ISR_echo(void)
-{
-    if(digitalRead(echoPin))  //transitioned to HIGH
-    {
-        pulseStart = micros();
-    }
-
-    else                      //transitioned to LOW
-    {
-        pulseEnd = micros();
-        state |= ECHO_RECD;
-    } 
-}
-
-void ISR_Rangefinder(void) { rangefinder.ISR_echo(); }
--- src/Rangefinder.h
+++ src/Rangefinder.h
@@ -3,61 +3,34 @@
 #include <Arduino.h>
 
 /** \class Rangefinder 
- * \brief A class to manage the HC-SR04 ultrasonic rangefinder. 
- * 
- * Uses a TRIG and ECHO pin to send chirps and detect round trip time.
- * 
- * The object rangefinder is declared extern to work with the ISRs, which 
- * means you must define your object with the same name:
- * 
- * Rangefinder rangefinder(<echo>, <trigger>);
+ * \brief Base class to manage the HC-SR04 distance sensors. 
+ * Manages a timer and general interface. Derived classes specialize for various sensors.
  * */
 class Rangefinder 
 {
 protected:
-    volatile uint8_t state = 0;
-
-    uint8_t echoPin = -1;
-    uint8_t trigPin = -1;
-
     // for keeping track of ping intervals
-    uint32_t lastPing = 0;     
+    uint32_t lastReading = 0;     
 
     // we set the pingInterval to 10 ms, but it won't actually ping that fast
     // since it _only_ pings if the ECHO pin is low -- that is, it will ping
     // in 10 ms or when the last echo is done, whichever is _longer_ 
-    uint32_t pingInterval = 10;    
-
-    // for keeping track of echo duration
-    volatile uint32_t pulseStart = 0;
-    volatile uint32_t pulseEnd = 0;
+    uint32_t readingInterval = 10;    
 
     // holds the last recorded distance
-    float distance = 99;
+    float distance = 999;
+
+    // checks to see if it's time to take a reading
+    bool checkReadingTimer(void);
 
 public:
-    Rangefinder(uint8_t echo, uint8_t trig);
+    Rangefinder(void);
 
-    // must call init() to set up pins and interrupts
-    void init(void);
-
-    // checks to see if it's time to emit a ping
-    uint8_t checkPingTimer(void);
-
-    // checks to see if an echo is complete
-    uint16_t checkEcho(void);
+    // must call init()
+    virtual void init(void) {}
 
     /** \brief Returns true upon receiving a new echo and false otherwise.
      * The last recorded distance will be stored in distance.
      * */
-    bool getDistance(float& distance);
-
-    // ISR for the echo pin
-    void ISR_echo(void);
+    virtual bool getDistance(float& distance) = 0;
 };
-
-// ISR for the echo
-void ISR_Rangefinder(void);
-
-// we declare as extern so we can refer to it in the ISR
-extern Rangefinder rangefinder;

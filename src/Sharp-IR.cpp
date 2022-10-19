365c17b7a26b0d2399dba92b350583ac44ab7e82
Greg <lewin.gc@gmail.com>
2022-10-19 14:21:02 -0400

    timer; baud rate

--- src/Sharp-IR.cpp
+++ src/Sharp-IR.cpp
@@ -1,12 +1,15 @@
 #include <Sharp-IR.h>
 
-const uint32_t SHARP_IR_INTERVAL = 40;
+const uint32_t SHARP_IR_INTERVAL = 1000;
 
 bool SharpIR::getDistance(float& distance)
 {
     bool newReading = false;
-    if(millis() - lastReading >= SHARP_IR_INTERVAL)
+    uint32_t currTime = millis();
+    if(currTime - lastReading >= SHARP_IR_INTERVAL)
     {
+        lastReading = currTime;
+
         uint16_t adcResult = analogRead(adcPin);
         distance = adcResult; //TODO
 
